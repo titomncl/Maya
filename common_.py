f86525cbe2ca5e48990a37062e9dfeeef93233c7
t.nicole <thomas-nicole@hotmail.fr>
2021-03-25 00:51:12 +0100

    add del pyc and versionning to force the env update

--- common_.py
+++ common_.py
@@ -11,7 +11,7 @@ from shiboken2 import wrapInstance
 
 from shutil import copyfile
 
-from Maya.globals import USER_PATH
+from Maya.globals import USER_PATH, DEV_PATH
 
 from Maya import init_env
 
@@ -64,8 +64,23 @@ def update_popup():
     choice_ = mc.confirmDialog(t=title, m=msg, b=btn, db=restart, cb=cancel, ds=cancel)
 
     if choice_ == restart:
+
+        maya_pyc()
+
         subprocess.Popen('"C:\\Program Files\\Autodesk\\Maya2019\\bin\\maya.exe"', shell=True)
         mc.quit(force=True)
+
+
+def maya_pyc():
+
+    maya_path = DEV_PATH + "/Maya/"
+
+    files_ = os.listdir(maya_path)
+
+    pyc_files = [file_ for file_ in files_ if ".pyc" in file_]
+
+    for pyc_file in pyc_files:
+        os.remove(os.path.join(maya_path, pyc_file))
 
 
 def get_main_window():

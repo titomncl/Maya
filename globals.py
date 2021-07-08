536df2a2901d0e75e03de1f0b64845b4ef1517da
t.nicole <thomas-nicole@hotmail.fr>
2021-07-08 12:47:31 +0200

    change PFE_ENV to PROJECT_ENV

--- globals.py
+++ globals.py
@@ -1,9 +1,9 @@
 import os
 
+
 MAYA_EXT = ".ma"
 
-USER_PATH = os.environ['USERPROFILE'].replace('\\', '/')
-PFE_PATH = os.environ["PFE_ENV"]
+PROJECT_PATH = os.environ["PROJECT_ENV"]
 DEV_PATH = os.environ["DEV_ENV"]
 ROOT_PATH = os.environ["ROOT_PATH"]
-PROJECT = PFE_PATH.split("/")[-1]
+PROJECT = PROJECT_PATH.split("/")[-1]

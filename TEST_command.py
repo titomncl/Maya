0d3ff840d98b30f3f35b6d63570edfc51cd9c663
Savitar46 <dehedinquentin@gmail.com>
2021-07-19 23:21:24 +0200

    Save;
    change bad script in good!

--- TEST_command.py
+++ TEST_command.py
@@ -1,17 +1,41 @@
 import re
 
+import maya.cmds as mc
+
 def maya_sel():
-    sel = mc.ls(sl=True)
+    sel = mc.ls(selection=True)
     return sel
 
-#def check(select):
-_name = (r"^(?P<asset>[A-Z]+)")
-_side = (r"(?P<side>C|L|R)")
-_object = (r"(?P<object>[A-Za-z0-9]+)")
-_type =  (r"(?P<type>jnt|jntEnd|geo|ctrl|loc|clstr|crv|nbs|ikhl|grp|parentConstraint|poleVectorConstraint)$")
+def check(selection):
+    """
 
-object_naming = re.compile(concat(_asset, _side, _object, _type, separator="_"))
+    Args:
+        selection (list(str)):
 
-<for each in maya_sel():
-    print(_name)
+    """
+    _asset = (r"^(?P<_asset>[A-Z]+)")
+    _side = (r"(?P<_side>C|L|R)")
+    _object = (r"(?P<_object>[A-Za-z0-9]+)")
+    _type = (r"(?P<_type>jnt|jntEnd|geo|ctrl|loc|clstr|crv|nbs|ikhl|grp|parentConstraint|poleVectorConstraint)$")
 
+    obj_name = re.compile(r"^(?P<_asset>\w+)_(?P<_loc>\w+)_(?P<_object>\w+)_(?P<_type>\w+)$")
+    grp_name = re.compile(r"^(?P<asset>[A-Z]+)_(?P<object>SKL|CTRL|XTRAS|GEO)_(?P<type>grp)$")
+
+    items_not_good = list()
+
+    for item in selection:
+        item_is_correct = obj_name.match(item)
+        item_grp_is_correct = grp_name.match(item)
+
+        if not item_is_correct and not item_grp_is_correct:
+            items_not_good.append(item)
+
+    return items_not_good
+
+
+nomenclature_error = check(maya_sel())
+
+if nomenclature_error:
+    print("theses were not good:", nomenclature_error)
+else:
+    print("it's all good")

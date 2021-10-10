019bac36df5ac5d1f88a00acf463073e367f558a
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-10-10 10:40:42 +0200

    ✨ turn exporter into Core object with staticmethods || add keys that get env var needed for the exporter

--- export/exporter.py
+++ export/exporter.py
@@ -1,59 +1,71 @@
 import os
-import re
 
 from CommonTools.concat import concat
-from Maya.common_ import export_alembic, get_filepath, get_root_for_abc_export, raise_error, smooth_selection, undo, \
-    get_objects
-from Maya.globals import PROJECT_PATH
+from CommonTools.os_ import make_dirs
+from Maya.common_ import export_alembic, get_root_for_abc_export, smooth_selection, undo, get_objects, \
+    get_cam_for_abc_export, frame_range
+
+try:
+    PATH = os.environ["ITEM_PUBLISH"]
+    NAME = os.environ["ITEM_NAME"]
+    TASK = os.environ["ITEM_TASK"]
+except KeyError:
+    raise RuntimeError("No scene loaded")
 
 
-def info_from_filepath():
-    path, filename = os.path.split(get_filepath())
-    filename, _ = os.path.splitext(filename)
+class Core:
 
-    filename_pattern = re.compile(r"^(?P<asset>[A-Z0-9_]+)_(?P<task>[A-Z]+)_?(?P<version>\d{3})?$")
-    path_pattern = re.compile(r".+(?P<type>CHARA|PROPS|SET|FX).+")
+    START_FRAME, END_FRAME = frame_range()
 
-    filename_match = filename_pattern.match(filename)
-    path_match = path_pattern.match(path)
+    @staticmethod
+    def compute_export_path(comment=""):
 
-    if filename_match and path_match:
-        filename_grp = filename_match.groupdict()
-        path_grp = path_match.groupdict()
+        if comment in [None, ""]:
+            filename = concat(NAME, TASK, separator="_")
+            path = os.path.join(PATH, NAME, TASK).replace("\\", "/")
+        else:
+            filename = concat(NAME, TASK, comment, separator="_")
+            path = os.path.join(PATH, NAME, TASK, comment).replace("\\", "/")
 
-        asset_name = filename_grp["asset"]
-        type_ = path_grp["type"]
-        task = filename_grp["task"]
+        make_dirs(path)
 
-        return asset_name, type_, task
+        filepath_ = concat(path, filename, separator="/")
+
+        return filepath_
+
+    @staticmethod
+    def compute_hd_export(start_frame=1, end_frame=1):
+        sel = get_objects(False)
+        smooth_selection(sel)
+        filepath = Core.compute_export_path(comment="HD")
+
+        Core.export_abc(filepath, start_frame, end_frame)
+
+        undo()
+
+    @staticmethod
+    def export_abc(filepath, start_frame=1, end_frame=1):
+
+        frame_range = "-frameRange {} {}".format(str(start_frame), str(end_frame))
+        abc_param = "-uvWrite -writeColorSets -writeFaceSets -worldSpace -writeVisibility -autoSubd -writeUVSets"
+        data_format = "-dataFormat ogawa"
+
+        if "CAMERA" in filepath:
+            root = get_cam_for_abc_export()
+        else:
+            root = get_root_for_abc_export()
+
+        if not root:
+            raise RuntimeError("No meshes to export")
+
+        command = "{} {} {} {} -file {}.abc".format(frame_range, abc_param, data_format, root, filepath)
+        export_alembic(command)
 
 
-def compute_export_path(name_, type_, task_, quality="LD"):
-    filename = concat(name_, task_, quality, separator="_")
-    filepath_ = concat(PROJECT_PATH, "DATA/LIB/PUBLISH", type_, name_, task_, quality, filename, separator="/")
-
-    return filepath_
+def main():
+    from CommonTools.exporter import controller
+    from Maya.common_ import get_main_window
 
 
-def compute_hd_export(name_, type_, task_, start_frame=1, end_frame=1):
-    sel = get_objects(False)
-    smooth_selection(sel)
-    filepath = compute_export_path(name_, type_, task_, quality="HD")
-
-    export_abc(filepath, start_frame, end_frame)
-
-    # undo(sel)
-
-
-def export_abc(filepath, start_frame=1, end_frame=1):
-
-    frame_range = "-frameRange {} {}".format(str(start_frame), str(end_frame))
-    abc_param = "-uvWrite -writeColorSets -writeFaceSets -worldSpace -writeVisibility -autoSubd -writeUVSets"
-    data_format = "-dataFormat ogawa"
-    root = get_root_for_abc_export()
-
-    if not root:
-        raise_error("No meshes to export")
-
-    command = "{} {} {} {} -file {}.abc".format(frame_range, abc_param, data_format, root, filepath)
-    export_alembic(command)
+    instance_ = controller.Controller(Core, get_main_window())
+    instance_.ui.show()

cc22cc77042d625d65b6afb6733eb0b7033dce19
Savitar46 <dehedinquentin@gmail.com>
2021-10-22 20:17:17 +0200

    Save; Non fonctionnel,
    -correction erreur ligne 12: (?°P°<_type°>°\w+)"
    -Ajout compile pour assembler la suite des mots.

---
+++ RenameToolOdin.py
@@ -0,0 +1,12 @@
+##renameToolCore##
+import maya.cmds as mc
+
+asset_name = 'assetname'
+location_name = 'location'
+object_name = 'objectname'
+## OBJECTIF FOR THIS TAKE SAME NAME OF 'EACH' OR MAKE A POSSIBILITY TO CHANGE OBJECT NAME, MAYBE WITH CHECK BOX ##
+type_name = 'typename'
+sl = True
+
+for each in sel:
+    mc.rename(each, asset_name + "_" + location_name + "_" + each + "_" + type_name)

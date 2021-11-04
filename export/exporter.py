adc7323e18d0871ed4d687b001c661187eef095e
t.nicole <thomas-nicole@hotmail.fr>
2021-11-04 09:25:33 +0100

    🐛 minor fix

--- export/exporter.py
+++ export/exporter.py
@@ -5,39 +5,52 @@ from CommonTools.os_ import make_dirs
 from Maya.common_ import export_alembic, get_root_for_abc_export, smooth_selection, undo, get_objects, \
     get_cam_for_abc_export, frame_range
 
-try:
-    PATH = os.environ["ITEM_PUBLISH"]
-    NAME = os.environ["ITEM_NAME"]
-    TASK = os.environ["ITEM_TASK"]
-except KeyError:
-    raise RuntimeError("No scene loaded")
 
-
-class Core:
+class Core(object):
 
     START_FRAME, END_FRAME = frame_range()
 
-    @staticmethod
-    def compute_export_path(comment=""):
+    @property
+    def path(self):
+        try:
+            return os.environ["ITEM_PUBLISH"]
+        except KeyError:
+            return ""
+
+    @property
+    def name(self):
+        try:
+            return os.environ["ITEM_NAME"]
+        except KeyError:
+            return ""
+
+    @property
+    def task(self):
+        try:
+            return os.environ["ITEM_TASK"]
+        except KeyError:
+            return ""
+
+    def compute_export_path(self, comment=""):
 
         if comment in [None, ""]:
-            filename = concat(NAME, TASK, separator="_")
-            path = os.path.join(PATH, NAME, TASK).replace("\\", "/")
+            filename = concat(self.name, self.task, separator="_")
+            path = os.path.join(self.path, self.name, self.task).replace("\\", "/")
         else:
-            filename = concat(NAME, TASK, comment, separator="_")
-            path = os.path.join(PATH, NAME, TASK, comment).replace("\\", "/")
+            filename = concat(self.name, self.task, comment, separator="_")
+            path = os.path.join(self.path, self.name, self.task, comment).replace("\\", "/")
 
         make_dirs(path)
 
         filepath_ = concat(path, filename, separator="/")
-
+        print(filepath_)
         return filepath_
 
     @staticmethod
     def compute_hd_export(start_frame=1, end_frame=1):
         sel = get_objects(False)
         smooth_selection(sel)
-        filepath = Core.compute_export_path(comment="HD")
+        filepath = Core.compute_export_path(Core(), comment="HD")
 
         Core.export_abc(filepath, start_frame, end_frame)
 

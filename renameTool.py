4e7c833a65d8a82bfa1b6b1b033116d3c5607d20
Savitar46 <dehedinquentin@gmail.com>
2021-11-05 19:48:12 +0100

    RenameTool:
    #Bug
    No save.py can't run saveTool

--- renameTool.py
+++ renameTool.py
@@ -1,11 +1,12 @@
 import maya.cmds as mc
+from Maya.tree.create_tree import ProjectTree
 
 def test():
     print("hello! :)")
 
 name = "cube"
 mySel = mc.ls(sl=True)
-assetName = "PROJECTNAME"
+assetName = ProjectTree().asset
 location = "C"
 type = "geo"
 

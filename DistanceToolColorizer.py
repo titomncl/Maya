dfbf5c91dfe0a4bafc564e81877875801a42cf8c
t.nicole <thomas-nicole@hotmail.fr>
2021-11-05 18:22:11 +0100

    🐛 fix undefined method to run script

--- DistanceToolColorizer.py
+++ DistanceToolColorizer.py
@@ -1,19 +1,35 @@
-##DistanceToolColorVisibilty##
+# DistanceToolColorVisibilty
 import maya.cmds as cmds
 
-Width = 20
-length = 4
-height = 18
 
-selection = cmds.ls(sl=True)
-for each in selection:
-    cmds.setAttr(each + '.overrideEnabled', 1)
+def color_distance_tool():
+    width = 20
+    length = 4
+    height = 18
 
-    if "width" in each:
-        cmds.setAttr(each + '.overrideColor', width)
+    selection = cmds.ls(sl=True)
+    for each in selection:
+        cmds.setAttr(each + '.overrideEnabled', 1)
 
-    elif "length" in each:
-        cmds.setAttr(each + '.overrideColor', length)
+        if "width" in each:
+            cmds.setAttr(each + '.overrideColor', width)
 
-    elif "height" in each:
-        cmds.setAttr(each+ '.overrideColor', height)
+        elif "length" in each:
+            cmds.setAttr(each + '.overrideColor', length)
+
+        elif "height" in each:
+            cmds.setAttr(each + '.overrideColor', height)
+
+
+if __name__ == '__main__':
+    import sys
+    from copy import copy
+
+    modules = copy(sys.modules)
+    for module in modules:
+        if "Maya" in module:
+            sys.modules.pop(module)
+
+    from Maya import common_ as cm
+
+    cm.test()
